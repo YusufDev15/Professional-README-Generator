f901e6c7a41b3f34d1e30f143ca99f6298bbb9c2
Yusuf <145934201+YusufDev15@users.noreply.github.com>
2023-12-19 20:55:22 +0000

    function to write READEME file and function to initialise program in index.js

--- starter/index.js
+++ starter/index.js
@@ -58,4 +58,19 @@ const questions = [
   },
 ];
 
+// // function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// // function to initialize program
+function init() {
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    console.log("Generating README...");
+    writeToFile("ExampleREADME.md", generateMarkdown({ ...inquirerResponses }));
+  });
+}
+
+// function call to initialize program
+init();
 
