e9984693243bc9da1086167acea7a3169f14d659
Yusuf <145934201+YusufDev15@users.noreply.github.com>
2023-12-19 20:59:30 +0000

    write function in Markdown for rendering license badge

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,19 +1,27 @@
-// Creating a function that creates a license link
-function renderLicenseLink(license) {
-  if (license !== "None") {
-    return `\n* [License](#license)\n`;
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    if (license !== "None") {
+      return `![Github License](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`;
+    }
+    return "";
   }
-  return "";
-}
-
-// Creating a function that returns the license section in the README
-function renderLicenseSection(license) {
-  if (license !== "None") {
-    return `## License  
+  
+  // Creating a function that creates a license link
+  function renderLicenseLink(license) {
+    if (license !== "None") {
+      return `\n* [License](#license)\n`;
+    }
+    return "";
+  }
+  
+  // Creating a function that returns the license section in the README
+  function renderLicenseSection(license) {
+    if (license !== "None") {
+      return `## License  
   
       Copyright @ ${license}. All rights reserved.
   
       Licensed under the ${license} license.`;
+    }
+    return "";
   }
-  return "";
-}
