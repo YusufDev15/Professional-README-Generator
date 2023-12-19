55cf65a7e01243aa08dbfa4dcd34f4e74b165848
Yusuf <145934201+YusufDev15@users.noreply.github.com>
2023-12-19 21:28:42 +0000

    create markdown function and thr table of contents

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,27 +1,47 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (license !== "None") {
-      return `![Github License](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`;
-    }
-    return "";
+  if (license !== "None") {
+    return `![Github License](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`;
   }
-  
-  // Creating a function that creates a license link
-  function renderLicenseLink(license) {
-    if (license !== "None") {
-      return `\n* [License](#license)\n`;
-    }
-    return "";
+  return "";
+}
+
+// Creating a function that creates a license link
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `\n* [License](#license)\n`;
   }
-  
-  // Creating a function that returns the license section in the README
-  function renderLicenseSection(license) {
-    if (license !== "None") {
-      return `## License  
+  return "";
+}
+
+// Creating a function that returns the license section in the README
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License  
   
       Copyright @ ${license}. All rights reserved.
   
       Licensed under the ${license} license.`;
-    }
-    return "";
   }
+  return "";
+}
+
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  by " " ${data.name}
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description 
+  ${data.description}
+  
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contributors)
+  * [Tests](#tests)
+  * [Questions](#questions)
+   ${renderLicenseLink(data.license)}
+
+   `;
+}
