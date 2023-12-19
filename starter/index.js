9b39a8519521bc80a02ed1540bcb6a814ecc1130
Yusuf <145934201+YusufDev15@users.noreply.github.com>
2023-12-19 20:50:59 +0000

    write array of questions for the user in index.js for the dynamically generated README

---
+++ starter/index.js
@@ -0,0 +1,61 @@
+const fs = require("fs"); //file system
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a short description of your project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide the languages and technologies used?",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please select a license you would like to use?",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "ISC", "BSD2", "none"],
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Provide a list of contributors if any?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "Provide your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Provide your full name?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Provide your email address?",
+  },
+];
+
+
